2eb98c5361c3942c6fed60258bb54cb318d66e30
Vitor Castro <81660618+vitorcastro018@users.noreply.github.com>
2022-11-21 13:59:15 -0300

    Add files via upload

---
+++ login_comercial.js
@@ -0,0 +1,10 @@
+// Pegar valores dos Inputs e função
+
+function logar(){
+
+    let usuario = document.getElementsById('usuario')
+    let senha = document.getElementById('senha')
+
+    alert(usuario + senha)
+
+}
